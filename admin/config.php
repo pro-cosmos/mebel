13016b584fcd8f14234193b8d07eeb06cdd50d14
Sboev Konstantin <ksboev@adyax.com>
2019-03-31 12:20:28 +0400

    Install ocStore

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://mebel.loc/admin/');
+define('HTTP_CATALOG', 'http://mebel.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://mebel.loc/admin/');
+define('HTTPS_CATALOG', 'http://mebel.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/mebel.loc/admin/');
+define('DIR_SYSTEM', '/var/www/mebel.loc/system/');
+define('DIR_IMAGE', '/var/www/mebel.loc/image/');
+define('DIR_STORAGE', '/var/www/storage1/');
+define('DIR_CATALOG', '/var/www/mebel.loc/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '111');
+define('DB_DATABASE', 'mebel.loc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
